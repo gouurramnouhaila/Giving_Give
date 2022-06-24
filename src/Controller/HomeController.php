7ac325f2d4e066adb66fde393bbbd77c294ab859
nouhailagourram <gourramnouha94@gmail.com>
2022-06-24 15:26:13 +0100

    update

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,18 +3,18 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/{reactRouting}", name="home", defaults={"reactRouting": null})
+     * @Route("/", name="home_app")
      */
-    public function index(): Response
+    public function index()
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        return $this->render('home/index.html.twig',[]);
     }
+    
 }
